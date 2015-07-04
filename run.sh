468b373f69519d13d76a42795134a94e575dba23
Hugh Perkins <hughperkins@gmail.com>
2015-07-04 18:46:06 +0800

    add dylib as a possible so suffix

--- run.sh
+++ run.sh
@@ -10,5 +10,6 @@ cmake ..
 make -j 4
 cd ..
 
-LUA_CPATH=build/lib?.so luajit test.lua
+LUA_CPATH='build/lib?.so;build/lib?.dylib' luajit test.lua
+LUA_CPATH='build/lib?.dylib;build/lib?.so' luajit test.lua
 
